66e0d01e04da95661a77ad59956f55de1bb441dc
meady35 <meady35@gmail.com>
2025-03-20 10:57:57 -0400

    Update main.js

--- html5up-eventually/assets/js/main.js
+++ html5up-eventually/assets/js/main.js
@@ -135,6 +135,40 @@
 				};
 
 			// Events.
+			// Telephon Form.
+		(function() {
+
+			// Vars.
+				var $form = document.querySelectorAll('#telephone-form')[0],
+					$submit = document.querySelectorAll('#telephone-form input[type="submit"]')[0],
+					$message;
+
+			// Bail if addEventListener isn't supported.
+				if (!('addEventListener' in $form))
+					return;
+
+			// Message.
+				$message = document.createElement('span');
+					$message.classList.add('message');
+					$form.appendChild($message);
+
+				$message._show = function(type, text) {
+
+					$message.innerHTML = text;
+					$message.classList.add(type);
+					$message.classList.add('visible');
+
+					window.setTimeout(function() {
+						$message._hide();
+					}, 3000);
+
+				};
+
+				$message._hide = function() {
+					$message.classList.remove('visible');
+				};
+
+			// Events.
 			// Note: If you're *not* using AJAX, get rid of this event listener.
 				$form.addEventListener('submit', function(event) {
 
